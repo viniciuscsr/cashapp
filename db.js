305e473b472dc8c9a01abac19c561b44d2fefc04
Vinícius Rodrigues <vinicius@viniciuss-mbp.myfiosgateway.com>
2020-07-12 11:31:40 -0400

    add connection string for DB

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_DATABASE,
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT,
 });
 
 module.exports = pool;
