e10fe6949d46f10b3ec4a0629f8c8846a892d3b6
Vinícius Rodrigues <vinicius@Viniciuss-MacBook-Pro.local>
2020-07-13 21:44:33 -0400

    fix local database

--- db.js
+++ db.js
@@ -1,15 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  // user: process.env.DB_USER,
-  // host: process.env.DB_HOST,
-  // database: process.env.DB_DATABASE,
-  // password: process.env.DB_PASSWORD,
-  // port: process.env.DB_PORT,
+  connectionString:
+    process.env.DATABASE_URL ||
+    'postgresql://node_user:node_password@localhost:5432/cashapp',
+  ssl: false,
 });
 
 module.exports = pool;
