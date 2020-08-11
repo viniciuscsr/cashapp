7109efe9e80c6bcbb88bb3504ef43d09dc7cba32
Vinicius Rodrigues <44007400+viniciuscsr@users.noreply.github.com>
2020-08-10 22:33:35 -0400

    created homepage

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 app.use(cookieParser());
 app.use(
   session({
